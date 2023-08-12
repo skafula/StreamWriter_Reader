ed9efa02419b2dc8959e29df03a1548ac0aece5e
skafula <79100340+skafula@users.noreply.github.com>
2023-08-12 14:39:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string filePath = @"c:\FileTest\europe.txt";
+        FileInfo fileInfo = new FileInfo(filePath);
+        //FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+        //new StreamWriter(filePath)
+        //new StreamWriter(fs)
+        using (StreamWriter sw = fileInfo.CreateText())
+        {
+            sw.WriteLine("Russia's population is about 137,000,000");
+            //some other code
+            sw.WriteLine("Hungary's population is about 9,000,000");
+        }
+        Console.WriteLine("europe.txt has been created.");
+
+        using (StreamReader streamReader = new StreamReader(filePath))
+        {
+            //Console.WriteLine(streamReader.ReadToEnd());
+
+            //Read text by characters
+            char[] buffer = new char[10];
+            int charCount = 0;
+            do
+            {
+                charCount = streamReader.Read(buffer, 0, 10);
+                string s1 = new string(buffer);
+                Console.WriteLine(s1);
+            } while (charCount > 0);
+        }
+    }
+}
